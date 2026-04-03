846aabb8818ac4ed9fde4056bf0714c18f8b9db0
Niklas Johansson <15849048+niklas-joh@users.noreply.github.com>
2026-04-03 22:45:15 +0100

    feat: implement SEO & Images admin pages with shared PostListTable (#61)

--- assets/seo/index.asset.php
+++ assets/seo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '76460c1ac10904ad0d31');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '0a9c916558d6956a07ab');
